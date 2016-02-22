e765ac23aebcb1bd53ca4fdd69e9fb61ba65faa2
dsaman <dsaman@GWC185F05.asurite.ad.asu.edu>
2016-02-22 15:29:25 -0700

    version1

---
+++ cse360/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,45 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		assertNotNull(0);
+	}
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetHistory() {
+		fail("Not yet implemented");
+	}
+
+
+
+}
+
